80ecf1dd346029492a9c7ce094e502a5c6462de1
Igor <ilaptev@ya.ru>
2021-10-23 12:07:31 +0300

    fix: change lang of calendar from ru to en

--- momentum/src/calendar.js
+++ momentum/src/calendar.js
@@ -2,9 +2,10 @@ const dateContainer = document.querySelector(".date")
 
 function getDate(){
   const date = new Date();
-  const days = ["Воскресение", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
+  /*const days = ["Воскресение", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]*/
+  days = ["Sunday", "Понедельник", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   const options = {month: 'long', day: 'numeric'};
-  const currentDate = date.toLocaleDateString('ru-Ru', options);
+  const currentDate = date.toLocaleString('en-En', options);
   const day = date.getDay();
   const res = `${days[day]}, ${currentDate}`;
   dateContainer.textContent = res;
